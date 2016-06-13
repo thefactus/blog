c96ec224518fba1a82819e7450952196c2343c62
Pablo Bello <thefactus@gmail.com>
2016-06-13 14:35:43 -0300

    Improving post specs

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,46 +7,30 @@ describe Post do
     it { should validate_presence_of :title }
     it { should validate_presence_of :body }
 
-    it 'is invalid' do
-      post.title = nil
-      post.body = nil
-      expect(post).to_not be_valid
-    end
-
-    it 'requires title' do
-      post.title = nil
-      expect(post).to_not be_valid
-      expect(post.errors[:title]).not_to be_blank
-    end
-
-    it 'requires body' do
-      post.body = nil
-      expect(post).to_not be_valid
-      expect(post.errors[:body]).not_to be_blank
-    end
-
     it 'title length should have at least 10 characters long' do
-      post.title = 'TITLE'
+      post.title = 'A' * 9
       expect(post).to_not be_valid
-      expect(post.errors.size).to eq(1)
+      expect(post.errors[:title]).to include(/is too short/)
     end
 
     it 'title length should be 255 characters max' do
       post.title = 'A' * 256
       expect(post).to_not be_valid
-      expect(post.errors.size).to eq(1)
+      expect(post.errors[:title]).to include(/is too long/)
     end
   end
 
   describe 'associations' do
     it { should belong_to :user }
+    it { should have_many :comments }
   end
 
   describe '#latest_posts' do
     it 'order by created_at desc' do
-      FactoryGirl.create(:post, title: 'First Title', body: 'lorem')
-      x = FactoryGirl.create(:post, title: 'Second Title', body: 'lorem')
-      expect(Post.latest_posts.first).to eq(x)
+      x = FactoryGirl.create(:post, title: 'First Title', body: 'lorem')
+      y = FactoryGirl.create(:post, title: 'Second Title', body: 'lorem')
+      z = FactoryGirl.create(:post, title: 'Third Title', body: 'lorem')
+      expect(Post.latest_posts).to eq([z, y, x])
     end
   end
 end
